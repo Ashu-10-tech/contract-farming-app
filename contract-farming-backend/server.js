5d3c6e9b963c8ba93cf2bfe1f446952a8ccb796c
Ashu-10-tech <ashunegi1425@gmail.com>
2025-07-25 14:26:19 +0530

    .

--- contract-farming-backend/server.js
+++ contract-farming-backend/server.js
@@ -15,12 +15,8 @@ app.use('/api', authRoutes);
 app.use('/api/tenders', tenderRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
-<<<<<<< HEAD:contract-farming-backend-main/server.js
   .then(() => {
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  }).catch(err => console.error(err));
-=======
-  .then(() => app.listen(process.env.PORT, () => console.log("Server running")))
-  .catch(err => console.log(err));
->>>>>>> 9ac634c6ce492e4944f27b44d93ab41bf4a3e9e3:contract-farming-backend/server.js
+  })
+  .catch(err => console.error(err));
